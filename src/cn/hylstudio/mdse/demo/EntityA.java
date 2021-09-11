6341d80dbff1b4f9d3456e7fbb1391ea9790b8a3
HuangYongliang <master@hylstudio.cn>
2021-09-12 01:55:14 +0800

    init

---
+++ src/cn/hylstudio/mdse/demo/EntityA.java
@@ -0,0 +1,6 @@
+package cn.hylstudio.mdse.demo;
+public class EntityA {
+	private String aaa;
+
+	private String bbb;
+}
---
+++ src/cn/hylstudio/mdse/demo/EntityB.java
@@ -0,0 +1,6 @@
+package cn.hylstudio.mdse.demo;
+public class EntityB {
+	private String ccc;
+
+	private String ddd;
+}
